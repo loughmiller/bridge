c2b0c45eb775db9fa36df7ab51631ecf72deecc9
Scott Loughmiller <scott@scalecomputing.com>
2023-08-23 14:28:00 -0700

    bridge working

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,18 @@
 #include <RH_ASK.h>
 
 
+////////////////////////////////////////////////////////////////////////////////
+// WIFI
+////////////////////////////////////////////////////////////////////////////////
+#define   MESH_PREFIX     "wizardMesh"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+Scheduler userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+void newConnectionCallback(uint32_t nodeId);
+bool connected = false;
 
 ////////////////////////////////////////////////////////////////////////////////
 // RF
@@ -22,6 +34,21 @@ byte messageType = 0;
 uint32_t messageData = 0;
 uint32_t timeSync = 0;
 
+// message types
+const byte typeCycle = 1;
+const byte typeBrightness = 2;
+const byte typeDensity = 3;
+const byte typeSparkles = 4;
+const byte typeHue = 5;
+const byte typeStreaks = 7;
+const byte typeSolid = 8;
+const byte typeSteal = 9;
+const byte typeSync = 10;
+const byte typeHue2 = 11;
+const byte typeCycle2 = 12;
+
+StaticJsonDocument<128> config;
+
 ////////////////////////////////////////////////////////////////////////////////
 // Setup
 ////////////////////////////////////////////////////////////////////////////////
@@ -30,22 +57,60 @@ void setup() {
   delay(1000);
   Serial.println("setup");
 
+// WIFI SETUP
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onNewConnection(&newConnectionCallback);
+  Serial.println("wifi setup complete");
+
   // Initialise radiohead
   if (!driver.init()) {
     Serial.println("init failed");
   } else {
-    Serial.println("init success");}
+    Serial.println("init success");
+  }
+
+  Serial.println("setup complete");
+  Serial.println("setup complete");
+  Serial.println("setup complete");
 }
 
 uint_fast8_t lastMessageID = 255;
+uint_fast32_t loggingTimestamp = 0;
 
 void loop() {
+  mesh.update();
   uint8_t buf[12];
   uint8_t buflen = sizeof(buf);
+
+  unsigned long localTime = millis();
+
+  if (localTime > loggingTimestamp + 2000) {
+    // Serial.println("logging");
+    loggingTimestamp = localTime;
+
+    if (mesh.getNodeList().size() == 0) {
+      if (connected) {
+        Serial.println("disconnected");
+      }
+      connected = false;
+    } else {
+      if (!connected) {
+        Serial.println("connected");
+      }
+      connected = true;
+    }
+  }
 
   if (driver.recv(buf, &buflen)) {
     if ((buf[0] != authByteStart) || (buf[buflen - 1] != authByteEnd)) {
       Serial.println("bad message");
+      Serial.print("Got: ");
+      for (uint_fast8_t i = 0; i < buflen; i++) {
+        Serial.print(buf[i]);
+        Serial.print(' ');
+      }
+      Serial.println();
       return;
     }
 
@@ -68,22 +133,90 @@ void loop() {
 
     messageType = buf[2];
 
-    if (messageType == 10 && buflen == 8) {
+    String json;
+
+    if (messageType == typeSync && buflen == 8) {
       // sync
       messageData = buf[3] << 24 | buf[4] << 16 | buf[5] << 8 | buf[6];
       // Serial.print("sync: ");
       // Serial.println(timeSync);
 
-    } else if (messageType > 0) {
-      messageData = buf[3];
+      // immediately send sync message
+      StaticJsonDocument<32> sync;
+      sync["sync"] = messageData;
+      serializeJson(sync, json);
+      mesh.sendBroadcast(json);
+      Serial.println(json);
+      return;
     }
 
-    StaticJsonDocument<32> doc;
-    doc["type"] = messageType;
-    doc["msg"] = messageData;
+    messageData = buf[3];
 
-    serializeJson(doc, Serial);
-    Serial.println();
+    // log message type and data
+    Serial.print("messageType: ");
+    Serial.println(messageType);
+    Serial.print("messageData: ");
+    Serial.println(messageData);
 
+    if (messageType == typeSteal) {
+      // immediately send steal message
+      StaticJsonDocument<64> steal;
+      steal["steal"] = messageData;
+      steal["hue"] = messageData;
+      steal["cycle"] = 0;
+      serializeJson(steal, json);
+      mesh.sendBroadcast(json);
+      Serial.println(json);
+      return;
+    }
+
+    // generate json config from message
+    switch (messageType)
+    {
+      case typeCycle:
+        config["cycle"] = messageData;
+        break;
+      case typeBrightness:
+        config["brightness"] = messageData;
+        break;
+      case typeDensity:
+        config["density"] = messageData;
+        break;
+      case typeSparkles:
+        config["sparkles"] = messageData;
+        break;
+      case typeHue:
+        config["hue"] = messageData;
+        config["cycle"] = 0;
+        break;
+      case typeStreaks:
+        config["streaks"] = messageData;
+        break;
+      case typeSolid:
+        config["solid"] = messageData;
+        break;
+      case typeHue2:
+        config["hue"] = messageData;
+        break;
+      case typeCycle2:
+        if (config["cycle"] > 0) {
+          config["cycle"] = messageData;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    serializeJson(config, json);
+    mesh.sendBroadcast(json);
+    Serial.println(json);
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+void newConnectionCallback(uint32_t nodeId) {
+}
+
